e7a62270376f5b31d2e331de54bb5df18cb5f575
trbenne2 <tyler_bennett25@yahoo.com>
2020-11-30 16:37:26 -0500

    sign up button component added to navbar.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,26 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import {Link} from 'react-router-dom'
 import {MdFingerprint} from 'react-icons/md'
 import {FaBars, FaTimes} from 'react-icons/fa'
+import {Button} from './Button'
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    }
+
+    window.addEventListener('resize', showButton);
+
     return (
         <Fragment>
             <div className="navbar">
@@ -12,9 +29,39 @@ function Navbar() {
                         <MdFingerprint className="navbar-icon"/>
                         Lavish
                     </Link>
-                    <div className="menu-icon">
-                        {}
+                    <div className="menu-icon" onClick={handleClick}>
+                        {click ? <FaTimes /> : <FaBars/>}
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to='/' className="nav-links">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/services' className="nav-links">
+                                Services
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/products' className="nav-links">
+                                Products
+                            </Link>
+                        </li>
+                        <li className="nav-btn">
+                            {button ? (
+                                <Link to="/sign-up" className="btn-link">
+                                    <Button buttonStyle="btn--outline">SIGN UP</Button>
+                                </Link>
+                            ) : (
+                                <Link to="/sign-up" className="btn-link">
+                                    <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                                        SIGN UP
+                                    </Button>
+                                </Link>
+                            )}
+                        </li>
+                    </ul>
                 </div>
             </div>
         </Fragment>
